5ee65345b6e05509c540a7420219297acdd660ca
Larisa Dolgova <larisa.dolgova@gmail.com>
2017-03-08 18:22:33 +0100

    Add config

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+
+module.exports = {
+  devtool: "sourcemap",
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "./build"),
+    publicPath: "/build/",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [{
+      test: /.js$/,
+      exclude: /node_modules/,
+      loader: "babel-loader"
+    }]
+  }
+}
