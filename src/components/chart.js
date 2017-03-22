7619514c34a3c74632dc5c0cf815571618b575e8
Larisa Dolgova <larisa.dolgova@gmail.com>
2017-03-22 12:44:53 +0100

    Don't draw the graph when "loading"

--- src/components/chart.js
+++ src/components/chart.js
@@ -14,19 +14,21 @@ const perfectData = [
 ];
 
 export const PredictionsChart = ({ predictions }) => (
-  <ResponsiveContainer aspect={1.5} width='100%'>
-    <LineChart data={calcStat(groupByProb(predictions))}
-      margin={{ top: 10, right: 10, left: 1, bottom: 40 }}>
-      <XAxis dataKey="conf" label={<Text dy={40} textAnchor="start">Confidence level</Text>} />
-      <YAxis label={<Text angle={270} verticalAnchor="start" textAnchor="end" width={300}>Correctness</Text>} />
-      <CartesianGrid strokeDasharray="3 3" />
-      <Tooltip labelFormatter={(label) => label + "% confidence level"} />
-      <Legend verticalAlign="top" height={36} />
-      <Line type="monotone" name="Your result" unit="%" dataKey="yours" stroke="#82ca9d" activeDot={{ r: 8 }} connectNulls={true} />
-      <Line name="Perfect calibration" unit="%" type="monotone" dataKey="perfect" stroke="#8884d8" />
-    </LineChart>
-  </ResponsiveContainer>
-)
+  predictions !== null && (
+    <ResponsiveContainer aspect={1.5} width='100%'>
+      <LineChart data={calcStat(groupByProb(predictions))}
+        margin={{ top: 10, right: 10, left: 1, bottom: 40 }}>
+        <XAxis dataKey="conf" label={<Text dy={40} textAnchor="start">Confidence level</Text>} />
+        <YAxis label={<Text angle={270} verticalAnchor="start" textAnchor="end" width={300}>Correctness</Text>} />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip labelFormatter={(label) => label + "% confidence level"} />
+        <Legend verticalAlign="top" height={36} />
+        <Line type="monotone" name="Your result" unit="%" dataKey="yours" stroke="#82ca9d" activeDot={{ r: 8 }} connectNulls={true} />
+        <Line name="Perfect calibration" unit="%" type="monotone" dataKey="perfect" stroke="#8884d8" />
+      </LineChart>
+    </ResponsiveContainer>
+  )
+) 
 
 const calcStat = (data) => R.map(
   level => Object.assign({}, level, { yours: data[level.conf] }),
@@ -42,12 +44,3 @@ const groupByProb = R.compose(
   R.groupBy(({ prob }) => prob),
   R.filter(({ correct }) => correct !== 'unknown'),
   R.values)
-
-// export const Statistics = ({ predictions }) =>
-//   <PredictionsChart data={calcStat(groupByProb(predictions))} />
-
-
-
-
-
-
