ba3331798472bda0a459ada3bbb1dba72e737648
Larisa Dolgova <larisa.dolgova@gmail.com>
2017-03-27 22:58:32 +0200

    Move card-params to another line on small screens

--- src/components/PredictionForm.js
+++ src/components/PredictionForm.js
@@ -14,23 +14,27 @@ export const PredictionForm = (props) => (
         required />
     </div>
     <div className="card-parameters">
-      <select name="prob" onChange={props.handleInputChange} value={props.currentPrediction.prob}>
-        <option value="50">50%</option>
-        <option value="60">60%</option>
-        <option value="70">70%</option>
-        <option value="80">80%</option>
-        <option value="90">90%</option>
-        <option value="95">95%</option>
-        <option value="97">97%</option>
-        <option value="99">99%</option>
-      </select>
-      <select type='checkbox' name="correct" onChange={props.handleInputChange} value={props.currentPrediction.correct}>
-        <option value="unknown">Unknown</option>
-        <option value="correct">Correct</option>
-        <option value="incorrect">Incorrect</option>
-      </select>
-      {props.edit && <DeleteButton id={props.id} handleDelete={props.handleDelete} />}
-      <button type='submit' className="badge btn-save-card"><i className="fa fa-check-square-o fa-lg" aria-hidden="true"></i></button>
+      <div className="card-delete">
+        {props.edit && <DeleteButton id={props.id} handleDelete={props.handleDelete} />}
+      </div>
+      <div className="card-change">
+        <select name="prob" onChange={props.handleInputChange} value={props.currentPrediction.prob}>
+          <option value="50">50%</option>
+          <option value="60">60%</option>
+          <option value="70">70%</option>
+          <option value="80">80%</option>
+          <option value="90">90%</option>
+          <option value="95">95%</option>
+          <option value="97">97%</option>
+          <option value="99">99%</option>
+        </select>
+        <select name="correct" onChange={props.handleInputChange} value={props.currentPrediction.correct}>
+          <option value="unknown">Unknown</option>
+          <option value="correct">Correct</option>
+          <option value="incorrect">Incorrect</option>
+        </select>
+        <button type='submit' className="badge btn-save-card"><i className="fa fa-check-square-o fa-lg" aria-hidden="true"></i></button>
+      </div>
     </div>
   </form >
 )
