c2176ddc1381624dca41ebb9244420ec44c58380
Sergei Dolgov <dolgovs@gmail.com>
2017-04-10 23:57:21 +0200

    Improve table (fixes #13)
    - Remove useless column
    - Compute numbers, not strings
    - Fix tests

--- src/components/Statistic.js
+++ src/components/Statistic.js
@@ -6,6 +6,7 @@ import { Table } from './Table';
 export const Statistic = ({ showTable, predictions, handleStatClick }) => (
   predictions !== null && (
     <div className='stat' onClick={handleStatClick}>
+      <h3>Results</h3>
       {showTable ?
         <Table predictions={predictions} /> :
         <PredictionsChart predictions={predictions} />
