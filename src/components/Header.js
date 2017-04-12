0a41725f9d1b1b9eccef85a262c73239a2bf8c0e
Larisa Dolgova <larisa.dolgova@gmail.com>
2017-04-12 21:03:57 +0200

    Align back/logout buttons with main container on wide screen

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { LogOutButton } from './buttons';
 
-export const Header = (props) =>
-  <div className="header">
-    {props.main
-      ? <LogOutButton logout={props.logout} />
-      : <button className="badge btn-close" onClick={props.handleCloseEdit}><i className="fa fa-arrow-left fa-2x" aria-hidden="true"></i></button>
-    }
-    <h1 className="title text-center">{props.text}</h1>
-
-  </div>
+export const Header = (props) => (
+  props.main
+    ?
+    <div className="header">
+      <div className="whitespace-left"></div>
+      <h1 className="title text-center">{props.text}</h1>
+      <LogOutButton logout={props.logout} />
+    </div>
+    :
+    <div className="header">
+      <button className="badge btn-close" onClick={props.handleCloseEdit}><i className="fa fa-arrow-left fa-2x" aria-hidden="true"></i></button>
+      <h1 className="title text-center">{props.text}</h1>
+      <div className="whitespace-right"></div>
+    </div>
+)
